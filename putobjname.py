d0e53f813a573ebd3f0195586576146c0dfa2441
Hideyo Yoshida <aquaviter@gmail.com>
2015-04-20 22:39:25 +0900

    update put.py

--- putobjname.py
+++ putobjname.py
@@ -1,11 +1,15 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 # _*_ coding: utf-8 _*_
 
+import urllib2
+import ast
 from boto.s3.connection import S3Connection
 
 bucket_name = 's3metasearch'
 
-conn = S3Connection()
+resp=urllib2.urlopen('http://169.254.169.254/latest/meta-data/iam/security-credentials/'+ bucket_name).read()
+resp=ast.literal_eval(resp)
+conn = S3Connection(resp['AccessKeyId'],resp['SecretAccessKey'])
 bucket = conn.get_bucket(bucket_name)
 
 for obj in bucket.list():
